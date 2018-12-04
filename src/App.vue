00720e8ae8fa964187a5f0bd117fb6573761f4e9
Eoin Traynor <eointraynor1@gmail.com>
2018-12-04 18:28:37 +0000

    Abstract basic example

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <template>
   <div id="app">
-    <Promised :promise="promise">
-      <!--
-        Use the "pending" slot for loading content
-      -->
-      <h1 slot="pending">Loading</h1>
-      <!-- The default scoped slots will be used as the result -->
-      <h1 slot-scope="data">Success! {{ data }}</h1>
-      <!-- The "rejected" scoped slot will be used if there is an error -->
-      <h1 slot="rejected" slot-scope="error">Error: {{ error.message }}</h1>
-    </Promised>
+    <basic-promised />
   </div>
 </template>
 
 <script>
-import { Promised } from 'vue-promised';
+import BasicPromised from './components/BasicPromised';
 
 export default {
   name: 'app',  
-  data() {
-    return {
-      promise: this.delay(),      
-    }
-  },
   components: {
-    Promised
-  },
-  methods: {
-    delay() {
-      return new Promise(resolve => setTimeout(() => resolve('boo'), 2000));
-    }
+    BasicPromised,
   }
 }
 </script>
---
+++ src/components/BasicPromised.vue
@@ -0,0 +1,32 @@
+<template>
+    <Promised :promise="promise">
+      <!--
+        Use the "pending" slot for loading content
+      -->
+      <h1 slot="pending">Loading</h1>
+      <!-- The default scoped slots will be used as the result -->
+      <h1 slot-scope="data">Success! {{ data }}</h1>
+      <!-- The "rejected" scoped slot will be used if there is an error -->
+      <h1 slot="rejected" slot-scope="error">Error: {{ error.message }}</h1>
+    </Promised>
+</template>
+
+<script>
+import { Promised } from 'vue-promised';
+
+export default {
+  components: {
+    Promised
+  },
+  data() {
+    return {
+      promise: this.delay(),      
+    }
+  },
+  methods: {
+    delay() {
+      return new Promise(resolve => setTimeout(() => resolve('Resolved!'), 2000));
+    }
+  }
+}
+</script>
